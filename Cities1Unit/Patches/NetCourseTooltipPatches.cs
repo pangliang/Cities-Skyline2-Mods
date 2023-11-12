acdced9ddaa991dd87d90b7d780657db8491db08
pangliang <418094911@qq.com>
2023-11-12 10:45:07 +0800

    The two patch methods do not need to use two classes

--- Cities1Unit/Patches/NetCourseTooltipPatches.cs
+++ Cities1Unit/Patches/NetCourseTooltipPatches.cs
@@ -4,11 +4,14 @@ using HarmonyLib;
 
 namespace Cities1Unit.Patches
 {
-    [HarmonyPatch(typeof(NetCourseTooltipSystem), "OnCreate")]
-    class NetCourseTooltipSystem_OnCreatePatch
+    [HarmonyPatch]
+    class NetCourseTooltipSystem_Patch
     {
         public static FloatTooltip m_Length_U;
-        static void Postfix(NetCourseTooltipSystem __instance)
+        
+        [HarmonyPatch(typeof(NetCourseTooltipSystem), "OnCreate")]
+        [HarmonyPostfix]
+        static void OnCreate_Postfix(NetCourseTooltipSystem __instance)
         {
             TooltipGroup m_Group = Traverse.Create(__instance).Field("m_Group").GetValue<TooltipGroup>();
             if(m_Group == null) {
@@ -25,18 +28,15 @@ namespace Cities1Unit.Patches
             m_Group.children.Add(m_Length_U);
 
         }
-    }
-
-    [HarmonyPatch(typeof(NetCourseTooltipSystem), "OnUpdate")]
-    class NetCourseTooltipSystem_OnUpdatePatch
-    {
-        static void Postfix(NetCourseTooltipSystem __instance)
+        
+        [HarmonyPatch(typeof(NetCourseTooltipSystem), "OnUpdate")]
+        [HarmonyPostfix]
+        static void OnUpdate_Postfix(NetCourseTooltipSystem __instance)
         {
             FloatTooltip m_Length = Traverse.Create(__instance).Field("m_Length").GetValue<FloatTooltip>();
-
-            if (NetCourseTooltipSystem_OnCreatePatch.m_Length_U != null && m_Length != null)
+            if (m_Length_U != null && m_Length != null)
             {
-                NetCourseTooltipSystem_OnCreatePatch.m_Length_U.value = m_Length.value / 8f; // Convert to Cities 1 units
+                m_Length_U.value = m_Length.value / 8f;
             }
         }
     }
